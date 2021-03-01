d72cc7f501d1ecd39850539621f9ec546b52a6f3
Nicole Stock <stockne@dukes.jmu.edu>
2021-03-01 10:16:25 -0500

    Fixing data for k=3 case

--- k_3_first_order/data/get_data_1.m
+++ k_3_first_order/data/get_data_1.m
@@ -2,7 +2,7 @@ function [z_vec_r,z_vec_th,z_vec_z,p_exact,f] = get_data_1(n)
 %GET_DATA1 Get z vector and p equation exact solution
 %   data1
 %   z = [ -sin(pi*z)(2r - 1)
-%         -sin(pi*z)(r - 1)
+%         -n*sin(pi*z)(r - 1)
 %         -pi*cos(pi*z)(r^2 - r) ]
 %   p = sin(pi*z)(r^2 - r)
 %   f = -2sin(pi*z) - (1/r)sin(pi*z)(2r - 1 - n^2(r - 1)) -
@@ -11,7 +11,7 @@ function [z_vec_r,z_vec_th,z_vec_z,p_exact,f] = get_data_1(n)
 % Date: Fall 2020
 
 z_vec_r = @(r,z) -sin(pi.*z).*(2.*r - 1);
-z_vec_th = @(r,z) -sin(pi.*z).*(r - 1);
+z_vec_th = @(r,z) -n.*sin(pi.*z).*(r - 1);
 z_vec_z = @(r,z) -pi*cos(pi.*z).*(r.^2 - r);
 p_exact = @(r,z) sin(pi.*z).*(r.^2 - r);
 f = @(r,z) -2.*sin(pi.*z) ...
